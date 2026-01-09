2382b6626d22ef8f21e613266221ad96575ec584
Diogenes <diogenesoliveiraa@gmail.com>
2026-01-09 12:07:08 -0300

    Troca de senha do postgres e ajuste de bugs

--- HERO_CLOUD/lgpd-back/utils/database.js
+++ HERO_CLOUD/lgpd-back/utils/database.js
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize("postgres", "postgres", "Diogenes100", {
+const sequelize = new Sequelize("postgres", "postgres", "Diogenes100.", {
   host: "localhost",
   dialect: "postgres",
   port: 5432,
